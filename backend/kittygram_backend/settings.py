1ea97b53997b00353cf6b4426d44c1240325459e
tawaluk <tawaluk@yandex.ru>
2023-08-30 18:20:54 +0300

    rev_2

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -104,7 +104,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'backend_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
